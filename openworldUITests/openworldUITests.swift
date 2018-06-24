ff35aac880a377bd1533ca6772d01b12921c3d35
navid Farshchi <navidfarshchi@navids-mac-mini.aussiecommerce.domain>
2018-06-25 09:49:42 +1000

    Initial Commit

---
+++ openworldUITests/openworldUITests.swift
@@ -0,0 +1,36 @@
+//
+//  openworldUITests.swift
+//  openworldUITests
+//
+//  Created by navid Farshchi on 25/6/18.
+//  Copyright © 2018 navid Farshchi. All rights reserved.
+//
+
+import XCTest
+
+class openworldUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
